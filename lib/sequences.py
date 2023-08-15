0671d710ed88dd8a13a1c20e372f91382a4e7d15
Demaris Ashmore <demiashmore@yahoo.com>
2023-08-15 11:32:36 -0600

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,11 @@
 
 def print_fibonacci(length):
     pass
+    fib = []
+    if length > 0:
+        fib.append(0)
+    if length >= 2:
+        fib.append(1)
+        for i in range(2, length):
+            fib.append(fib[-1] + fib[-2])
+    print(fib)
